f560c731ee56fac30e1fb411b2bead54176dc5b7
Isaias <isaiaslflores@gmail.com>
2025-02-20 10:41:05 -0500

    crud

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,5 +1,94 @@
-export default function NewPage() {
+"use client"
+
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function NewPage({params}:any) {
+  // const { id } = await params;
+  const router = useRouter();
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  useEffect(() => {
+    if(params.id) {
+      fetch(`/api/tasks/${params.id}`)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data)
+          setTitle(data.title),
+          setDescription(data.description)
+        });
+    }
+  }, []);
+
+  const onSubmit = async (e:any) => {
+    //React.FormEvent<HTMLFormElement>
+    e.preventDefault();
+    if(params.id) {
+      const res = await fetch(`/api/tasks/${params.id}`, {
+        method: "PUT",
+        body: JSON.stringify({title, description}),
+        headers: {
+          'Content-type':'application/json'
+        }
+      });
+      const data = await res.json();
+      console.log(data);
+    } else {
+      const res = await fetch('/api/tasks', {
+        method: 'POST',
+        body: JSON.stringify({title, description}),
+        headers: {
+          'Content-type':'application/json'
+        }
+      });
+      const data = await res.json();
+      console.log(data);
+    }
+    // router.refresh();
+    router.push("/");
+  }
+
   return (
-    <div>NewPage</div>
+    <div className="h-screen flex justify-center items-center">
+      <form className="bg-slate-800 p-10 md:w-1/2 lg:w-1/3" onSubmit={onSubmit}>
+        <label htmlFor="title" className="font-bold text-sm">Task title</label>
+        <input 
+          type="text" 
+          id="title"
+          className="border border-gray-400 p-2 mb-4 w-full text-black" 
+          placeholder="Title"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
+        <label htmlFor="description" className="font-bold text-sm">Task description</label>
+        <textarea 
+          rows={3} 
+          id="description"
+          className="border border-gray-400 p-2 mb-4 w-full text-black" 
+          placeholder="Describe your task"
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
+        ></textarea>
+        <div className="flex justify-between">
+          <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Create</button>
+          {params.id && (
+            <button 
+              type="button" 
+              className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" 
+              onClick={async () => {
+                const res = await fetch(`/api/tasks/${params.id}`, {
+                  method: "DELETE"
+                });
+                const data = await res.json();
+                router.push("/");
+              }}
+            >
+              Delete
+            </button>
+          )}
+        </div>
+      </form>
+    </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
-export default function Home() {
+import TaskCard from "@/components/TaskCard";
+import { prisma } from "@/libs/prisma"
+
+async function loadTasks() {
+  return await prisma.task.findMany();
+}
+
+export default async function Home() {
+  const tasks = await loadTasks();
   return (
-    <div>
-      <div>Tasks</div>
-    </div>
+    <section className="container mx-auto">
+      <div className="grid grid-cols-3 gap-3">
+        {tasks.map((task) => (
+          <TaskCard task={task} key={task.id}/>
+        ))}
+      </div>  
+    </section>
   );
 }
